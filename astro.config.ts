14155eadd96aac4e629319747f2cd63588b02e35
michael faith <michaelfaith@users.noreply.github.com>
2026-05-05 05:56:44 -0500

    docs: add Getting Started page (#1781)
    <!-- 👋 Hi, thanks for sending a PR to eslint-plugin-package-json! 🗂
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1779
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This change populates our Getting Started page, using the dedicated
    `getting-started.md` I extracted out as the primary source. I used the
    1.0 version of it, so I wouldn't have to come back and change it again
    (updated Node and ESLint version compat)
    
    <img width="1400" height="1613" alt="screenshot"
    src="https://github.com/user-attachments/assets/0db3d916-4a2a-427f-964f-a1bc96f69249"
    />
    
    ---------
    
    Co-authored-by: Josh Goldberg ✨ <git@joshuakgoldberg.com>

---
+++ astro.config.ts
@@ -0,0 +1,47 @@
+import starlight from "@astrojs/starlight";
+import starlightCatppuccin from "@catppuccin/starlight";
+import { defineConfig } from "astro/config";
+
+const site = "https://esling-plugin-package-json.dev";
+
+export default defineConfig({
+  experimental: {
+    clientPrerender: true,
+  },
+  integrations: [
+    starlight({
+      favicon: "/images/favicon.svg",
+      logo: {
+        dark: "./src/assets/logo/logo-dark.svg",
+        light: "./src/assets/logo/logo-light.svg",
+      },
+      plugins: [starlightCatppuccin()],
+      sidebar: [
+        { label: "Getting Started", slug: "getting-started" },
+        {
+          autogenerate: { directory: "rules" },
+          label: "Rules",
+        },
+      ],
+      social: [
+        {
+          href: "https://github.com/michaelfaith/eslint-plugin-package-json",
+          icon: "github",
+          label: "GitHub",
+        },
+      ],
+      title: "ESLint Plugin: Package JSON",
+    }),
+  ],
+  outDir: "./dist-site",
+  prefetch: {
+    defaultStrategy: "hover",
+  },
+  publicDir: "./site/public",
+  root: "./site",
+  server: {
+    port: 3000,
+  },
+  site,
+  srcDir: "./site/src",
+});
