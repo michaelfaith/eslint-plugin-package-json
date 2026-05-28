9269b147f709c373a105ee7907bf9a15e61bc1df
michael faith <michaelfaith@users.noreply.github.com>
2026-05-28 17:42:09 -0500

    refactor(experimental): minor tweak to language (#1890)
    <!-- 👋 Hi, thanks for sending a PR to eslint-plugin-package-json! 🗂
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [ ] Addresses an existing open issue: fixes #000
    - [ ] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [ ] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->

--- src/experimental/plugin.ts
+++ src/experimental/plugin.ts
@@ -4,6 +4,8 @@ import { toCompatRule } from 'eslint-json-compat-utils';
 
 import { plugin as originalPlugin } from '../plugin.ts';
 
+const language = 'json/json';
+
 const rules = Object.fromEntries(
   Object.entries(originalPlugin.rules).map(([name, rule]) => [
     name,
@@ -14,14 +16,14 @@ const rules = Object.fromEntries(
 // Add `meta.languages` to enforce use of the json/json language.
 for (const rule of Object.values(rules)) {
   // Manually add the `languages` property in the types, since ESLint v9 types doesn't include it (resulting in a type check error in CI).
-  (rule.meta as { languages?: string[] }).languages ??= ['json/json'];
+  (rule.meta as { languages?: string[] }).languages ??= [language];
 }
 
 export const plugin = {
   configs: {
     recommended: {
       files: ['**/package.json'],
-      language: 'json/json',
+      language,
       name: 'package-json/experimental-recommended',
       plugins: {
         get 'package-json'(): ESLint.Plugin {
@@ -32,7 +34,7 @@ export const plugin = {
     },
     stylistic: {
       files: ['**/package.json'],
-      language: 'json/json',
+      language,
       name: 'package-json/experimental-stylistic',
       plugins: {
         get 'package-json'(): ESLint.Plugin {
