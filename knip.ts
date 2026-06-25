45d97d601a5ff21b623f8103103f4147b9acf3eb
michael faith <michaelfaith@users.noreply.github.com>
2026-06-25 16:22:47 -0500

    fix knip config

--- knip.ts
+++ knip.ts
@@ -4,5 +4,6 @@ export default {
   entry: ['src/**/*.test.*'],
   ignoreDependencies: ['@eslint/json'],
   ignoreExportsUsedInFile: { interface: true, type: true },
+  ignoreUnresolved: ['^~/'],
   project: ['src/**/*.ts'],
 } satisfies KnipConfig;
